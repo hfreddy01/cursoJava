f06b9417459e0419040113889e755211a9a7daac
Freddy <hfreddy01@gmail.com>
2022-02-10 11:05:30 -0400

    Se agrega modulo ejercicios al proycto

---
+++ src/cursoJava/ejercicios/HolaMundo.java
@@ -0,0 +1,11 @@
+package cursoJava.ejerciciosResueltos.otros;
+
+public class EjemploString {
+
+	public static void main(String[] args) {
+		String text = "Hola mundo desde Java...";
+		System.out.println(text);
+
+	}
+
+}
