5cce0bc97c8e3a3b04031741882c5cd72fc77f8d
Freddy Herrera <hfreddy01@gmail.com>
2022-09-18 12:53:54 -0400

    se actualiza proyecto cursoJava

--- src/cursoJava/ejerciciosResueltos/bucles/For/EjerciciosBucleFor3.java
+++ src/cursoJava/ejerciciosResueltos/bucles/For/EjerciciosBucleFor3.java
@@ -6,8 +6,8 @@ import javax.swing.JOptionPane;
  * 
  * @author cursoJava
  * 
- * 3 - ESCRIBIR UN PROGRAMA QUE LEA 10 NOTAS DE ALUNNOS Y NOS INFORME CUANTOS TIENEN NOTAS MAYORES O IGUALES A 7
- *     Y CUANTOS MENORES.
+ *         3 - ESCRIBIR UN PROGRAMA QUE LEA 10 NOTAS DE ALUNNOS Y NOS INFORME
+ *         CUANTOS TIENEN NOTAS MAYORES O IGUALES A 7 Y CUANTOS MENORES.
  *
  */
 
@@ -15,34 +15,27 @@ public class EjerciciosBucleFor3 {
 
 	// METODO PRINCIPAL DE LA CLASE.
 	public static void main(String[] args) {
-		// MENSAJE.
-		JOptionPane.showMessageDialog(null, " 3 - ESCRIBIR UN PROGRAMA QUE LEA 10 NOTAS DE ALUNNOS Y NOS INFORME CUANTOS "
-		+ "TIENEN NOTAS MAYORES O IGUALES A 7\r\n" + "Y CUANTOS MENORES.");
-		
 		// CREAMOS VARIABLES.
 		String text;
-		int notas,aprobados, reprobados;
+		int notas, aprobados, reprobados;
 		// INICIALIZAMOS VARIABLES.
 		aprobados = 0;
 		reprobados = 0;
 		// VALIDAMOS.
-		for(int i=1; i<=10; i++) {
+		for (int i = 1; i <= 10; i++) {
 			// INGRESMOS DATOS POR VENTANA DE DIALOGO.
 			text = JOptionPane.showInputDialog("Ingrese notas");
 			// TRANSFORMAMOS DE INT A STRING.
-			notas = Integer.parseInt(text);		
+			notas = Integer.parseInt(text);
 			// VALIDAMOS.
-			if(notas > 7) {
+			if (notas > 7) {
 				aprobados++;
 			} else {
 				reprobados++;
-			}	
+			}
 		}
-		
 		// IMPRIMIMOS.
-		System.out.println("Cantidad de aprobados : " +aprobados);
-		System.out.println("Cantidad de reprobados : " +reprobados);
-
+		System.out.println("Cantidad de aprobados : " + aprobados);
+		System.out.println("Cantidad de reprobados : " + reprobados);
 	}
-
 }
