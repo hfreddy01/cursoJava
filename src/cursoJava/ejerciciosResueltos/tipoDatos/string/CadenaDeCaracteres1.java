206e6d09bf3e4bbd93a963ae25b02ce13ee21f5d
Freddy Herrera <hfreddy01@gmail.com>
2022-09-20 15:58:50 -0400

    Se garega la clase CadenaCaracteres1 al proyecto cursoJava

--- src/cursoJava/ejerciciosResueltos/tipoDatos/string/CadenaDeCaracteres1.java
+++ src/cursoJava/ejerciciosResueltos/tipoDatos/string/CadenaDeCaracteres1.java
@@ -1,16 +1,42 @@
 package cursoJava.ejerciciosResueltos.tipoDatos.string;
+
+import java.util.Scanner;
+
 /**
  * 
  * @author cursoJava
  * 
- * Solicitar el ingreso del nombre y edad de dos personas. Mostrar el nombre de la persona con mayor edad.
+ *         EJERCICIOS CADENA DE CARACTERES :
+ * 
+ *         SOLICITAR EL INGRESO DEL NOMBRE Y EDAD DE DOS PERSONAS. MOSTRAR EL
+ *         NOMBRE DE LA PERSONA CON MAYOR EDAD.
+ * 
  *
  */
+
 public class CadenaDeCaracteres1 {
 
+	// METODO PRINCIPAL DE LA CLASE.
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	 // CREAMOS EL OBJETO DE LA CLASE SCANNER PARA LA ENTRADA DE VALORES POR TECLADO.
+		Scanner teclado = new Scanner(System.in);
+		// CREAMOS VARIABLES.
+		String nombre1, nombre2;
+		int edad1, edad2;
+		// INGRESAMOS DATOS POR TECLADO.
+		System.out.println("Ingrese nombre : ");
+		nombre1 = teclado.next();
+		System.out.println("Ingrese edad : ");
+		edad1 = teclado.nextInt();
+		System.out.println("Ingrese nombre : ");
+		nombre2 = teclado.next();
+		System.out.println("Ingrese edad : ");
+		edad2 = teclado.nextInt();
+		// VALIDAMOS.
+		if(edad1 > edad2) {
+			System.out.println("La persona con mayor edad es : "+nombre1 +" con " +edad1 +" años");
+		} else {
+			System.out.println("La persona con mayor edad es : "+nombre2 +" con " +edad2 +" años");
+		}
 	}
-
 }
